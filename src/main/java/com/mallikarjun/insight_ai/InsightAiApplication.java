016caf6c1e8d462d1b8dae99457b0ef9fea5152c
mallikarjun-halagali <88491807+mallikarjun-halagali@users.noreply.github.com>
2025-12-29 09:24:34 +0530

    Add initial project structure with Maven configuration and application setup

---
+++ src/main/java/com/mallikarjun/insight_ai/InsightAiApplication.java
@@ -0,0 +1,13 @@
+package com.mallikarjun.insight_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightAiApplication.class, args);
+	}
+
+}
